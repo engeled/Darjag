359ac44a151628b82eebca1bcb7be55e50595f30
engeled <angel_soriazapata@hotmail.com>
2026-05-31 00:29:42 -0500

    Springboot

---
+++ src/main/java/lorem/ipsum/darjag/DarjagApplication.java
@@ -0,0 +1,13 @@
+package lorem.ipsum.darjag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DarjagApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DarjagApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lorem/ipsum/darjag/DarjagApplicationTests.java
@@ -0,0 +1,13 @@
+package lorem.ipsum.darjag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DarjagApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
